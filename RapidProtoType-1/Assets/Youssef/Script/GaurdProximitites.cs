e73858a6c2f4e954a1885f49c3a2406a87240a7f
youssef waeil <ywaeil938@gmail.com>
2024-12-06 00:27:20 +0400

    Finished Gaurd patrol scripit

---
+++ RapidProtoType-1/Assets/Youssef/Script/GaurdProximitites.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaurdProximitites : MonoBehaviour
+{
+    //points where the gaurd will patrol in
+    public Transform pA;
+    public Transform pB;
+    private Transform currentP;
+
+    //properties of the AI
+    public float speed = 2f;
+    public float waitingTime = 10f;
+    public float proximityAngle = 45f;
+    public float proximityRadius = 5f;
+    private bool isWaiting = false;
+
+    //the player
+    public Transform player;
+ 
+    void Start()
+    {
+        currentP = pA;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isWaiting) 
+        {
+            // then carry one ur patrol duty
+        }
+        
+        //search for player 
+    }
+
+    void CarryOn()
+    {
+        // gaurd moves to the target
+        transform.position = Vector3.MoveTowards(transform.position, currentP.position, speed * Time.deltaTime);
+
+        //check IF the gaurd has REACHED the Points , which ar pA, pB
+        if(Vector3.Distance(transform.position, currentP.position) < 0.1f)
+        {
+            // the gaurd looks at prsion cell and then rotates
+            StartCoroutine(WaitandTurn());
+        }
+
+    }
+
+    IEnumerator WaitandTurn()
+    {
+        isWaiting = true;
+
+        //wait the specifeid time given
+        yield return new WaitForSeconds(waitingTime);
+
+        //rotate
+        transform.Rotate(0, 90, 0);
+
+        // switch points
+        currentP = (currentP == pA) ? pB : pA;
+
+        isWaiting = false;
+    }
+
+    public void OnDestroy()
+    {
+        // get vector from garud to player
+        Vector3 playerPosition = player.position - transform.position;
+
+        //is player in proximity? IF yes THEN....
+        if(playerPosition.magnitude < proximityRadius) 
+        {
+            //get the angle
+            float playersAngle = Vector3.Angle(transform.forward, playerPosition);
+
+            if(playersAngle <= proximityAngle / 2 ) 
+            {
+                Debug.Log("u failed");
+            }
+        }
+
+    }
+
+}
